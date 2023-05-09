dbd269e61f611d736ee23e4545e9ae7b9c9410d8
parastoo gol mohammadi <parastoogmohammadi@gmail.com>
2023-05-09 12:47:11 -0400

    update files.

--- flask_app.py
+++ flask_app.py
@@ -12,9 +12,9 @@ from mlxtend.frequent_patterns import fpgrowth
 from mlxtend.frequent_patterns import association_rules
 import pandas as pd
 import dash
-import dash_table as dt
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dash_table as dt
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
